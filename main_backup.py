bd5fef8bf80f20b15b28aa7cfac90d8d5661bcf3
ppsha3 <52101237+ppsha3@users.noreply.github.com>
2019-08-12 07:22:55 -0400

    Add files via upload

--- main_backup.py
+++ main_backup.py
@@ -1,4 +1,13 @@
+"""
+Changes to make:
+1) string out of range in reveal_word
+2) Separate Classes to different files
+3) Create event handler
+
+"""
+
 import pygame
+import random
 
 
 class Cursor():
@@ -7,125 +16,152 @@ class Cursor():
 
         self.draw()
 
+
     def firstCursorPos(self):
 
-        start_x = self.button.button_location[0] + 5
-        start_y = self.button.button_location[1] + 5
+        start_x = self.button.button_location[0] + 6
+        start_y = self.button.button_location[1] + 6
 
-        start_point = (start_x, start_y)
+        self.start_point = [start_x, start_y]
 
         end_x = start_x
-        end_y = start_y + self.button.button_size[1] - 10
+        end_y = start_y + self.button.button_size[1] - 12
 
-        end_point = (end_x, end_y)
-        
-        return (start_point, end_point)
+        self.end_point = [end_x, end_y]
 
-    def displayCursorText(self, text):
 
-        text_area = text.get_size()
+    def updateCursorPos(self, input_text):
 
-        self.start_point = (self.start_point[0] + text_area[0], self.start_point[1])
-        
+        input_surf = game_font.render(input_text, True, black)
+        text_area = input_surf.get_size()
+
+        start_x = self.start_point[0] + text_area[0] + 2
+        start_y = self.start_point[1]
+
+        self.start_point = (start_x, start_y)
+
+        self.end_point[0] = start_x
+#	    self.end_point[1] = self.start_point[1]
+
         self.draw()
 
         return True
 
+
     def __init__(self, input_button):
 
         self.button = input_button
-        self.start_point, self.end_point = self.firstCursorPos()
+        self.firstCursorPos()
         self.cursor_active = False
+
 
     def draw(self):
 
         pygame.draw.aaline(gameDisplay, black, self.start_point, self.end_point)
-        
+
 
 class Animation():
+
 
     def setStartPoint(self):
 
         current_point = [None, None]
-    
+
         if self.direction == 'top':
             current_point[0] = self.destination[0]
             current_point[1] = 0
 
         if self.direction == 'bottom':
             current_point[0] = self.destination[0]
-            current_point[1] = 0
-            
+            current_point[1] = display_height
+
         return current_point
 
 
     def __init__(self, surface, destination, direction = None, speed = None):
-    
+
         self.surface = surface
         self.destination = destination
-    
-        self.speed = speed
-        
+        self.surface_size = self.surface.get_size()
+
         if direction == None:
-            self.current_point = []
             self.done = True
         else:
             self.direction = direction
             self.current_point = self.setStartPoint()
             self.done = False
-        
-    
+            self.speed = speed
+
+
     def getNextPoint(self):
-        
+
         if self.direction == 'top':
-            self.current_point[0] = self.destination[0] 
             self.current_point[1] = self.current_point[1] + self.speed
             if self.current_point[1] >= self.destination[1]:
                 self.current_point[1] = self.destination[1]
 
         if self.direction == 'bottom':
-            self.current_point[0] = self.destination[0]
-            self.current_point[1] = 0
-            
-        return self.current_point
+            self.current_point[1] = self.current_point[1] - self.speed
+            if self.current_point[1] <= self.destination[1]:
+                self.current_point[1] = self.destination[1]
 
-    def getSize(self):
 
-        return self.surface.get_size()
-    
+    def animate(self):
+
+        if not self.done:
+            gameDisplay.blit(self.surface, self.current_point)
+            self.getNextPoint()
+            if self.destination <= self.current_point:
+                self.done = True
+        else:
+            gameDisplay.blit(self.surface, self.destination)
+
+        return self.done
+
+
+    def updateSurface(self, word):
+
+        new_font = pygame.font.Font(None, 40)
+        self.surface = game_font.render(word, True, black)
+        self.surface_size = self.surface.get_size()
 
 
 class Button():
 
-    def setText(self, text):
+
+    def setText(self, text, align):
 
         self.text = text
         self.txt_surf = game_font.render(self.text, True, black)
 
         txt_width, txt_height = self.txt_surf.get_size()
-        mid_point_x = self.button_location[0] + (self.button_size[0]/2)
-        mid_point_y = self.button_location[1] + (self.button_size[1]/2)
 
-        self.txt_loc = (mid_point_x - (txt_width/2), mid_point_y - (txt_height/2))
+        if align == 'center':
+            x_point = self.button_location[0] + (self.button_size[0]/2) - (txt_width/2)
+            y_point = self.button_location[1] + (self.button_size[1]/2) - (txt_height/2)
+
+        if align == 'left':
+            x_point = self.button_location[0] + 5
+            y_point = self.button_location[1] + 5
+
+        self.txt_loc = (x_point, y_point)
+
+        gameDisplay.blit(self.txt_surf, self.txt_loc)
 
 
-    def __init__(self, colour, button_location, button_size = None):        
+    def __init__(self, colour, button_location, button_size):
 
         self.button_location = button_location
         self.colour = colour
-
-        if button_size != None :
-            self.button_size = button_size
-        else:
-            self.button_size = [40, 30]
+        self.button_size = button_size
 
         self.light_colour = (colour[0] - 25, colour[1] - 25, colour[2] - 25)
-        
+
 
     def draw(self):
 
         self.rect = pygame.draw.rect(gameDisplay, self.colour, (self.button_location[0], self.button_location[1], self.button_size[0], self.button_size[1]), 2)
-    
+
 
     def hover(self):
 
@@ -142,25 +178,6 @@ def play_music(file_name):
     music.set_volume(0.25)
 
 
-def animate(animation_obj):
-
-    if not animation_obj.done:
-        gameDisplay.blit(animation_obj.surface, animation_obj.current_point)
-        current_point = animation_obj.getNextPoint()
-        if animation_obj.destination <= current_point:
-            animation_obj.done = True
-    else:
-        gameDisplay.blit(animation_obj.surface, animation_obj.destination)
-        
-    return animation_obj.done
-
-
-def take_input():
-
-        cursor.cursor_active = True
-            
-
-
 def game_intro():
 
 ##    play_music('music/intro_music.mp3')
@@ -169,18 +186,17 @@ def game_intro():
     welcome_obj = Animation(welcome_surf, [130, 75], 'top', 5)
 
     start_button = Button(grey, (335, 450), (100, 50))
-    start_button.setText('Start')
 
     intro = True
 
     while intro:
-	
+
         gameDisplay.fill(white)
 
-        if animate(welcome_obj):
+        if welcome_obj.animate():
 
             start_button.draw()
-            gameDisplay.blit(start_button.txt_surf, start_button.txt_loc)
+            start_button.setText(text='Start', align='center')
 
             for event in pygame.event.get():
 ##                print(event)
@@ -193,20 +209,20 @@ def game_intro():
                 if event.type == pygame.MOUSEMOTION:
                     if start_button.rect.collidepoint(mouse_position):
                         start_button.hover()
-                                                                       
+
                 if event.type == pygame.MOUSEBUTTONUP:
                     if start_button.rect.collidepoint(mouse_position):
                         intro = False
 
         pygame.display.update()
         clock.tick(30)
-		
+
     return True
 
-def parseInput(key_name):
 
-    if key_name in range(3,7):
+def parseInput(input_text):
 
+    if int(input_text) in range(3,8):
         return True
 
     return False
@@ -214,7 +230,7 @@ def parseInput(key_name):
 
 def handle_events():
 
-    global input_button, cursor, text
+    global input_button, cursor, input_text
 
     for event in pygame.event.get():
 ##                print(event)
@@ -226,12 +242,11 @@ def handle_events():
         mouse_position = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEMOTION:
             if input_button.rect.collidepoint(mouse_position):
-                input_button.hover()  
-                                                           
+                input_button.hover()
+
         if event.type == pygame.MOUSEBUTTONUP:
             if input_button.rect.collidepoint(mouse_position):
                 cursor.cursor_active = True
-                text = ''
             else:
                 if cursor.cursor_active == True:
                     cursor.cursor_active = False
@@ -239,52 +254,195 @@ def handle_events():
         if cursor.cursor_active == True and event.type == pygame.KEYDOWN:
             key_name = pygame.key.name(event.key)
             if key_name == 'return':
-                parseInput(text)
-                text = ''
-                print(pygame.key.name(event.key))
-            else:
-                text = text + key_name
+                if parseInput(input_text):
+                    cursor.cursor_active = False
+                    return True
+            elif key_name == 'backspace':
+                input_text = ''
+                cursor.firstCursorPos()
+            elif len(input_text) == 0:
+                input_text = key_name
+                cursor.updateCursorPos(input_text)
+            else:   pass
+
+    return False
 
 
 def start_game():
 
     global input_button, cursor, input_text
 
-    pygame.mixer.fadeout(2000)
-
+##  pygame.mixer.fadeout(2000)
 ##  play_music('music/game_music.mp3')
 
     ok_text_surf = game_font.render('OK!', True, black)
     ok_text = Animation(ok_text_surf, [375, 50], 'top', 3)
-	
-    set_text_surf = game_font.render('Set the length of the word[3-7]  ', True, black)
-    set_text = Animation(set_text_surf, [200,130])
 
-    text_size = set_text.getSize()
-    input_button = Button(grey, (set_text.destination[0] + text_size[0], set_text.destination[1]))
+    set_text_surf = game_font.render('Set the length of the word between 3 to 7 ', True, black)
+    set_text = Animation(set_text_surf, [170,130])
+
+    text_size = set_text.surface.get_size()
+    button_location = (set_text.destination[0] + text_size[0], set_text.destination[1] - 5)
+    input_button = Button(grey, button_location, [40, 30])
 
     cursor = Cursor(input_button)
-    input_text = game_font.render('x', True, black)
+    input_text = ''
 
     started = False
-
     while not started:
-
         gameDisplay.fill(white)
 
-        if animate(ok_text):
-            animate(set_text)
+        if ok_text.animate():
+            set_text.animate()
+
             input_button.draw()
+
+            if handle_events():
+                started = True
 
             if cursor.cursor_active == True:
                 cursor.blink()
 
-            handle_events()
-            cursor.displayCursorText(input_text)
-			
+            if len(input_text) > 0:
+                input_button.setText(input_text, align='left')
+
         pygame.display.update()
         clock.tick(30)
-	
+
+    return input_text
+
+
+def generate_word(len_word):
+
+    filepath = "words/word" + str(len_word) + ".txt"
+
+    with open("{}".format(filepath),"r") as wordfile:
+
+        words = wordfile.readlines()
+        random_word = random.choice(words)
+
+    return random_word
+
+
+def reveal_word(current_guess, current_word, word):
+
+    new_word = ''
+
+    for i in range(len(word)):
+        if current_guess == word[i]:
+            current_word = current_word.replace(current_word[i], current_guess, 1)
+        new_word = new_word + current_word[i] + ' '
+
+    print(current_word)
+    print(new_word)
+
+    return current_word, new_word
+
+
+def parse_guess(current_guess):
+
+    if current_guess.lower().isalpha():
+        return True
+    if current_guess == '*':
+        reveal_hint()
+        return True
+
+
+def getCenter(obj, width = None, height = None):
+
+    if width == None:
+        width = display_width
+
+    if height == None:
+        height = display_height
+
+    x_point = width/2 - obj[0]/2
+    y_point = height/2 - obj[1]/2
+
+    return (x_point, y_point)
+
+
+def play(word_len):
+
+    new_font = pygame.font.Font(None, 40)
+
+    word = generate_word(word_len)
+    print(word)
+
+    current_word = '*' * word_len
+    disp_curr_word = '* ' * word_len
+    current_word_text = new_font.render(disp_curr_word, True, black)
+    center_point = getCenter(current_word_text.get_size(), height=95)
+    current_word_obj = Animation(current_word_text, center_point)
+
+    guess_text = new_font.render('Guess the word', True, black)
+    guess_obj = Animation(guess_text, [300, 100])
+
+    input_button = Button(grey, [375, 150], [40, 30])
+    cursor = Cursor(input_button)
+
+    previous_guess = []
+    current_guess = ''
+
+    play = True
+
+    while play:
+
+        gameDisplay.fill(white)
+
+        pygame.draw.aaline(gameDisplay, black, (0, 75), (display_width, 75))
+
+        current_word_obj.animate()
+        guess_obj.animate()
+
+        input_button.draw()
+
+        for event in pygame.event.get():
+##            print(event)
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            mouse_position = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEMOTION:
+                if input_button.rect.collidepoint(mouse_position):
+                    input_button.hover()
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                if input_button.rect.collidepoint(mouse_position):
+                    cursor.cursor_active = True
+                else:
+                    if cursor.cursor_active == True:
+                        cursor.cursor_active = False
+
+            if cursor.cursor_active == True and event.type == pygame.KEYDOWN:
+                key_name = pygame.key.name(event.key)
+                if key_name == 'return':
+                    if parse_guess(current_guess):
+                        print(current_guess, current_word, word)
+                        current_word, new_word = reveal_word(current_guess, current_word, word)
+                        current_word_obj.updateSurface(new_word)
+                    current_guess = ''
+                    cursor.firstCursorPos()
+                elif key_name == 'backspace':
+                    current_guess = ''
+                    cursor.firstCursorPos()
+                elif len(current_guess) == 0:
+                    current_guess = key_name
+                    cursor.updateCursorPos(current_guess)
+                else:   pass
+
+
+        if cursor.cursor_active == True:
+            cursor.blink()
+
+        if len(current_guess) > 0:
+            input_button.setText(current_guess, align='left')
+
+        clock.tick(30)
+        pygame.display.update()
+
     return True
 
 
@@ -301,9 +459,9 @@ white = (255,255,255)
 grey = (150,150,200)
 light_grey = (75,75,100)
 
-game_font = pygame.font.Font(None, 35)
+game_font = pygame.font.Font(None, 30)
 clock = pygame.time.Clock()
 
 game_intro()
-start_game()
-#play()
+word_len = start_game()
+play(int(word_len))
